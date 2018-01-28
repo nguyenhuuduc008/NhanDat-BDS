71e5fb3eedc8c30a079858b4c42ffedbad742515
DESKTOP-R2HHNBE\Nguyen Huu Duc <nguyenhuuduc0008@gmail.com>
2018-01-28 10:26:42 +0700

    Refactor code

--- App/src/app/bds/add_edit/edit-media.js
+++ App/src/app/bds/add_edit/edit-media.js
@@ -1,12 +1,12 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	angular.module('app.bds')
-	.controller('MediaCtrl', MediaCtrl)
-    .controller("ViewFileCtrl", ViewFileCtrl);
+    angular.module('app.bds')
+        .controller('MediaCtrl', MediaCtrl)
+        .controller("ViewFileCtrl", ViewFileCtrl);
 
-	/** @ngInject */
-	function MediaCtrl($rootScope,$q, $scope, $stateParams,  $state,$filter,$ngBootbox,$window,$uibModal, authService,currentAuth, bdsMediaService,appUtils,toaster) {
+    /** @ngInject */
+    function MediaCtrl($rootScope, $q, $scope, $stateParams, $state, $filter, $ngBootbox, $window, $uibModal, authService, currentAuth, bdsMediaService, appUtils, toaster) {
         $rootScope.settings.layout.showSmartphone = false;
         $rootScope.settings.layout.showPageHead = true;
         $rootScope.settings.layout.guestPage = false;
@@ -54,10 +54,10 @@
                 url: './app/bds/add_edit/_tab-media.tpl.html'
             }
         };
-          
+
         vm.keyword = '';
         vm.filter = 'All';
-	    vm.groupedItems = [];
+        vm.groupedItems = [];
         vm.filteredItems = [];
         vm.pagedItems = [];
         vm.paging = {
@@ -80,54 +80,54 @@
             firebaseStorage: true,
             parallelUploads: 10
         };
-    
+
         vm.dzCallbacks = {
-            'addedfile': function(file){
+            'addedfile': function (file) {
 
-        },
-            'success': function(file, response){
-            var snapshot  = {};
-            _.each(response, function(task, key){
-                snapshot[key] = task.snapshot;
-            });
-            var metadata =snapshot.ori.metadata;
-            var imagemedia = {
-                "alternativeText": "",
-                "author": currentUser.email,
-                "bucket": metadata.bucket,
-                "caption": "",
-                "description": "",
-                "displayName": metadata.name,
-                "downloadUrl": snapshot.ori.downloadURL,
-                "fileName": metadata.name,
-                "fileSize": metadata.size,
-                "fileType": metadata.contentType,
-                "fullPath": metadata.fullPath,
-                "lowRes": {
-                    "downloadUrl": snapshot.lowres ? snapshot.lowres.downloadURL: '',
-                    "fileName": snapshot.lowres ? snapshot.lowres.metadata.name: '',
-                    "fullPath": snapshot.lowres ? snapshot.lowres.metadata.fullPath: ''
-                },
-                "storageLocation": 'gs://'+metadata.bucket+'/'+metadata.fullPath,					
-                "thumbnail": {
-                    "downloadUrl": snapshot.thumb ? snapshot.thumb.downloadURL : '',
-                    "fileName": snapshot.thumb ? snapshot.thumb.metadata.name : '',
-                    "fullPath": snapshot.thumb ? snapshot.thumb.metadata.fullPath : ''
-                },
-                "type": 'image',
-                "originalName": file.name || '',
-                "bdsId": bdsId
-            };
-
-            bdsMediaService.addFile(bdsId, imagemedia).then(function(res){
-                if(!res.result){
-                    toaster.error(res.errorMsg);
-                    return;
-                }
-                vm.search('');
-            });
             },
-            'error': function(file, err){
+            'success': function (file, response) {
+                var snapshot = {};
+                _.each(response, function (task, key) {
+                    snapshot[key] = task.snapshot;
+                });
+                var metadata = snapshot.ori.metadata;
+                var imagemedia = {
+                    "alternativeText": "",
+                    "author": currentUser.email,
+                    "bucket": metadata.bucket,
+                    "caption": "",
+                    "description": "",
+                    "displayName": metadata.name,
+                    "downloadUrl": snapshot.ori.downloadURL,
+                    "fileName": metadata.name,
+                    "fileSize": metadata.size,
+                    "fileType": metadata.contentType,
+                    "fullPath": metadata.fullPath,
+                    "lowRes": {
+                        "downloadUrl": snapshot.lowres ? snapshot.lowres.downloadURL : '',
+                        "fileName": snapshot.lowres ? snapshot.lowres.metadata.name : '',
+                        "fullPath": snapshot.lowres ? snapshot.lowres.metadata.fullPath : ''
+                    },
+                    "storageLocation": 'gs://' + metadata.bucket + '/' + metadata.fullPath,
+                    "thumbnail": {
+                        "downloadUrl": snapshot.thumb ? snapshot.thumb.downloadURL : '',
+                        "fileName": snapshot.thumb ? snapshot.thumb.metadata.name : '',
+                        "fullPath": snapshot.thumb ? snapshot.thumb.metadata.fullPath : ''
+                    },
+                    "type": 'image',
+                    "originalName": file.name || '',
+                    "bdsId": bdsId
+                };
+
+                bdsMediaService.addFile(bdsId, imagemedia).then(function (res) {
+                    if (!res.result) {
+                        toaster.error(res.errorMsg);
+                        return;
+                    }
+                    vm.search('');
+                });
+            },
+            'error': function (file, err) {
                 console.log('upload img error');
                 console.log(file);
                 console.log(err);
@@ -135,11 +135,11 @@
         };
 
         //Functions
-        vm.loadTab = function(key){
+        vm.loadTab = function (key) {
             vm.activeTab = key;
             $state.go('bds.' + key, { bdsId: vm.bdsId });
         };
-        
+
         vm.groupToPages = function () {
             vm.pagedItems = [];
             for (var i = 0; i < vm.filteredItems.length; i++) {
@@ -149,17 +149,17 @@
                     vm.pagedItems[Math.floor(i / vm.paging.pageSize)].push(vm.filteredItems[i]);
                 }
             }
-            if( vm.filteredItems.length % vm.paging.pageSize === 0){
+            if (vm.filteredItems.length % vm.paging.pageSize === 0) {
                 vm.paging.totalPage = vm.filteredItems.length / vm.paging.pageSize;
-            }else{
+            } else {
                 vm.paging.totalPage = Math.floor(vm.filteredItems.length / vm.paging.pageSize) + 1;
             }
         };
 
         vm.search = function (keyword) {
             bdsMediaService.searchMedia(bdsId, keyword).then(function (result) {
-                vm.filteredItems = appUtils.sortArray(result,'timestampCreated');
-                vm.paging.totalRecord = result.length; 
+                vm.filteredItems = appUtils.sortArray(result, 'timestampCreated');
+                vm.paging.totalRecord = result.length;
                 vm.paging.currentPage = 0;
                 //group by pages
                 vm.groupToPages();
@@ -167,7 +167,7 @@
         };
 
         vm.search('');
-          
+
         //$scope Functions
         $scope.changePage = function () {
             vm.groupToPages();
@@ -176,62 +176,66 @@
         vm.sortBy = function (sortBy) {
             vm.orderByDesc = !vm.orderByDesc;
             vm.filteredItems = _.orderBy(vm.filteredItems, ['timestampCreated'], [sortBy]);
-            vm.paging.totalRecord = vm.filteredItems.length; 
+            vm.paging.totalRecord = vm.filteredItems.length;
             vm.paging.currentPage = 0;
             //group by pages
             vm.groupToPages();
         };
 
-        vm.filterItems = function(){
+        vm.filterItems = function () {
             vm.keyword = '';
-            bdsMediaService.getMediaBDS(bdsId).then(function(data){
+            bdsMediaService.getMediaBDS(bdsId).then(function (data) {
                 var lst = bdsMediaService.filterItems(data, 'All', vm.filter);
-                vm.filteredItems = appUtils.sortArray(lst,'timestampCreated');
-                vm.paging.totalRecord = lst.length; 
+                vm.filteredItems = appUtils.sortArray(lst, 'timestampCreated');
+                vm.paging.totalRecord = lst.length;
                 vm.paging.currentPage = 0;
                 //Group by pages
                 vm.groupToPages();
             });
         };
 
-        vm.delete = function(item){
+        vm.delete = function (item) {
             var self = this;
             $ngBootbox.confirm('Are you sure want to delete ' + item.displayName + '?')
-            .then(function() {
-               bdsMediaService.removeMedia(bdsId,item.$id).then(function(res){
-                    if(res.result){
-                        toaster.pop('success','Success', "Delete Successful!");  
-                        vm.search(''); 
-                    }else{
-                        toaster.pop('error','Error', "Delete  Error! " + res.errorMsg);       
-                    }
-                }); 
-            }, function() {
+                .then(function () {
+                    bdsMediaService.removeMedia(bdsId, item.$id).then(function (res) {
+                        if (res.result) {
+                            toaster.pop('success', 'Success', "Delete Successful!");
+                            vm.search('');
+                        } else {
+                            toaster.pop('error', 'Error', "Delete  Error! " + res.errorMsg);
+                        }
+                    });
+                }, function () {
+                });
+        };
+        vm.save = function (item) {
+            appUtils.showLoading();
+            bdsMediaService.updateMedia(bdsId, item.$id, item).then(function (res) {
+                appUtils.hideLoading();
+                if (res.result) {
+                    $scope.$apply(function () {
+                        toaster.pop('success', 'Success', "Save Change Successful!");
+                    });
+                } else {
+                    $scope.$apply(function () {
+                        toaster.pop('error', 'Error', "Change Error! " + res.errorMsg);
+                    });
+                }
             });
         };
-        vm.save = function(item){
-            appUtils.showLoading();
-            bdsMediaService.updateMedia(bdsId, item.$id, item).then(function(res){
-                appUtils.hideLoading();
-                if(res.result){
-                    toaster.pop('success','Success', "Save Change Successful!");  
-                }else{
-                    toaster.pop('error','Error', "Change Error! " + res.errorMsg);       
-                }
-            }); 
-        };
-        vm.fileSrc = function(item){
+        vm.fileSrc = function (item) {
             return appUtils.mediaImgSrc(item, '100px');
         };
 
-        vm.cutString = function(text){
+        vm.cutString = function (text) {
             if (text.length > 40) {
                 return text.substring(0, 40) + ' ...';
             }
             return text;
         };
 
-        vm.cutTypeString = function(text){
+        vm.cutTypeString = function (text) {
             if (text.length > 20) {
                 return text.substring(0, 20) + ' ...';
             }
@@ -265,7 +269,7 @@
         //         toaster.warning("Please choose some items to execute action!");
         //         return;
         //     }
-            
+
         //     $ngBootbox.confirm('Are you sure want to apply ' + actionTxt + ' action as selected?').then(function(){
         //         if(action === 'delete'){
         //             appUtils.showLoading();
@@ -306,20 +310,20 @@
         //     });
         // };
 
-        
 
-       
-	}
-    
+
+
+    }
+
     /** @ngInject */
-    function ViewFileCtrl($rootScope, $scope, $state,$sce, $uibModalInstance,item) {
+    function ViewFileCtrl($rootScope, $scope, $state, $sce, $uibModalInstance, item) {
         $scope.item = item;
-        $scope.type = $scope.item .type.split('/')[0];
-        if($scope.type == "video"){
-            $scope.videoLink =  $sce.trustAsResourceUrl($scope.item.downloadUrl);
+        $scope.type = $scope.item.type.split('/')[0];
+        if ($scope.type == "video") {
+            $scope.videoLink = $sce.trustAsResourceUrl($scope.item.downloadUrl);
         }
 
-         $scope.close = function () {
+        $scope.close = function () {
             $uibModalInstance.dismiss('cancel');
         };
     }

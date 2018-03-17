b802bcdef812adf6a25dc23729e35a8f44fb7573
Vinh Tran <vinhtranit08@gmail.com>
2018-03-17 09:37:54 +0700

    #comment - change UI text

--- App/src/app/quyTrinhPhapLy/quyTrinhPhapLy-list.Ctr.js
+++ App/src/app/quyTrinhPhapLy/quyTrinhPhapLy-list.Ctr.js
@@ -11,7 +11,7 @@
         var currentUser = $rootScope.storage.currentUser;
         var quyTrinhPhapLyListVm =this;// jshint ignore:line
         quyTrinhPhapLyListVm.items=[];
-        quyTrinhPhapLyListVm.selectAction = 'Bulk Actions';
+        quyTrinhPhapLyListVm.selectAction = 'Chọn';
         quyTrinhPhapLyListVm.selectAll = function(controlId, name){
             appUtils.checkAllCheckBox(controlId,name);
         };
@@ -43,18 +43,18 @@
                     lstIds.push($(this).val() + '');
                 }
             });
-            var removeIndex = quyTrinhPhapLyListVm.selectAction.indexOf('Delete');
+            var removeIndex = quyTrinhPhapLyListVm.selectAction.indexOf('Xóa');
             if(removeIndex === -1){
                 appUtils.hideLoading();
-                toaster.warning("Please choose action to execute!");
+                toaster.warning("Chọn hành động để thực thi!");
                 return;
             } 
             if(lstIds.length <= 0){
                 appUtils.hideLoading();
-                toaster.warning("Please choose some items to execute action!");
+                toaster.warning("Vui lòng chọn quy trình bạn muốn xóa!");
                 return;
             }
-            $ngBootbox.confirm('Are you sure want to apply ' + quyTrinhPhapLyListVm.selectAction + ' action as selected?')
+            $ngBootbox.confirm('Bạn có chắc muốn ' + quyTrinhPhapLyListVm.selectAction + ' quy trình này?')
             .then(function() {
                 var removePromise=[];
                 _.forEach(lstIds, function(id){
@@ -62,7 +62,7 @@
                 });
                 $q.all(removePromise).then(function(rs){
                     appUtils.hideLoading();
-                    toaster.success("Delete success!");
+                    toaster.success("Xóa thành công!");
                     init();
                 });
             }, function() {
